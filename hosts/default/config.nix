755a423c85d671449788039d9d429c2fd2acaf05
ezzobir <ezzobir_bezziou@protonmail.com>
2024-08-18 14:07:41 +0100

    add caps2esc and touchpad configs

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -342,6 +342,17 @@ in
 
   # Services to start
   services = {
+    # Map CapsLock to Esc on single press and Ctrl on when used with multiple keys.
+    services.interception-tools = {
+      enable = true;
+      plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+      udevmonConfig = ''
+        - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+          DEVICE:
+            EVENTS:
+              EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+      '';
+    };
     xserver = {
       enable = false;
       xkb = {
@@ -367,7 +378,14 @@ in
       enable = false;
       autodetect = true;
     };
-    libinput.enable = true;
+    libinput = {
+      enable = true;
+      touchpad = { 
+        middleEmulation = true;
+        naturalScrolling = true;
+        tapping = true;
+        };
+    };
     fstrim.enable = true;
     gvfs.enable = true;
     openssh.enable = true;
