11fa29d5a29dbc6b64a2dcaf43ca836f6c7b3a10
installer <installer@gmail.com>
2024-08-20 22:38:46 +0100

    switch gruvbox dark hard theme

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -54,27 +54,48 @@ in
   # Styling Options
   stylix = {
     enable = true;
-    image = ../../config/wallpapers/flora.jpg;
-    # base16Scheme = "{pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    image = ../../config/wallpapers/wave.jpg;
     # base16Scheme = "{pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yml";
+    #gruvbox-dark-hard 
     base16Scheme = {
-      base00 = "1e1e2e";
-      base01 = "181825";
-      base02 = "313244";
-      base03 = "45475a";
-      base04 = "585b70";
-      base05 = "cdd6f4";
-      base06 = "f5e0dc";
-      base07 = "b4befe";
-      base08 = "f38ba8";
-      base09 = "fab387";
-      base0A = "f9e2af";
-      base0B = "a6e3a1";
-      base0C = "94e2d5";
-      base0D = "89b4fa";
-      base0E = "cba6f7";
-      base0F = "f2cdcd";
+      base00 = "1d2021";
+      base01 = "3c3836";
+      base02 = "504945";
+      base03 = "665c54";
+      base04 = "bdae93";
+      base05 = "d5c4a1";
+      base06 = "ebdbb2";
+      base07 = "fbf1c7";
+      base08 = "fb4934";
+      base09 = "fe8019";
+      base0A = "fabd2f";
+      base0B = "b8bb26";
+      base0C = "8ec07c";
+      base0D = "83a598";
+      base0E = "d3869b";
+      base0F = "d65d0e";
+
     };
+    # base16Scheme = "{pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    # catppuccin-mocha
+    # base16Scheme = {
+    #   base00 = "1e1e2e";
+    #   base01 = "181825";
+    #   base02 = "313244";
+    #   base03 = "45475a";
+    #   base04 = "585b70";
+    #   base05 = "cdd6f4";
+    #   base06 = "f5e0dc";
+    #   base07 = "b4befe";
+    #   base08 = "f38ba8";
+    #   base09 = "fab387";
+    #   base0A = "f9e2af";
+    #   base0B = "a6e3a1";
+    #   base0C = "94e2d5";
+    #   base0D = "89b4fa";
+    #   base0E = "cba6f7";
+    #   base0F = "f2cdcd";
+    # };
     polarity = "dark";
     opacity.terminal = 0.8;
     cursor.package = pkgs.bibata-cursors;
