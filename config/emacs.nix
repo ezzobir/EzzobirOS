4c9d08896a533b1956cd503f45700b7ff8758586
installer <installer@gmail.com>
2024-08-21 17:05:54 +0100

    enable emacs

--- config/emacs.nix
+++ config/emacs.nix
@@ -1,12 +1,16 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
+
+  services.emacs.enable = true;
+
   programs.emacs = {
     enable = true;
+    package = pkgs.emacs29-pgtk;
+    extraPackages = epkgs: [
+      epkgs.vterm
+      epkgs.treesit-grammars.with-all-grammars
+    ];
   };
-  services= {
-    emacs = {
-      enable = true;
-      client.enable = true;
-    };
-  };
+
+
 }
