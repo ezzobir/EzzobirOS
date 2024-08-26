7681e7ba9519ab7876032b83b00a3f3e107eeb0d
installer <installer@gmail.com>
2024-08-26 11:15:34 +0100

    fixing toggle waybar script

--- scripts/toggle-waybar.nix
+++ scripts/toggle-waybar.nix
@@ -1,7 +1,7 @@
 
 { pkgs, ... }:
 pkgs.writeShellScriptBin "toggle-waybar" ''
-  if pgrep -x ".waybar-wrapped" > /dev/null; then
+  if pgrep -x "waybar" > /dev/null; then
       pkill -x waybar
       sleep 1
   else
