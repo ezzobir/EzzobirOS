ff799964834b06c066dc30bbdce196b6b3730c06
installer <installer@gmail.com>
2024-08-26 09:06:51 +0100

    add script to toggle waybar

--- scripts/toggle-waybar.nix
+++ scripts/toggle-waybar.nix
@@ -1,28 +1,12 @@
-# { pkgs }:
-# 
-# pkgs.writeShellScriptBin "rofi-launcher" ''
-#   if pgrep -x "rofi" > /dev/null; then
-#     # Rofi is running, kill it
-#     pkill -x rofi
-#     exit 0
-#   fi
-#   rofi -show drun
-# ''
-
 
 { pkgs, ... }:
 pkgs.writeShellScriptBin "toggle-waybar" ''
-  if ps aux | grep "[w]aybar" > /dev/null; then
+  if pgrep -x "waybar" > /dev/null; then
       pkill -x "waybar"
+      sleep 1
   else
       waybar &
   fi
 ''
 
-    # if pgrep -x "waybar" > /dev/null; then
-    #     pkill -x "waybar"
-    #     sleep 1
-    # else
-    #     waybar &
-    # fi
 
