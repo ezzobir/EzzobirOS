093a506a6ae1f93804d77ff98a4ecb43782e61fb
installer <installer@gmail.com>
2024-09-08 19:04:23 +0100

    emacs-overlay

--- flake.nix
+++ flake.nix
@@ -4,28 +4,28 @@
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     # home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     # stylix.url = "github:danth/stylix/release-24.05";
     stylix.url = "github:danth/stylix";
+
     fine-cmdline = {
       url = "github:VonHeikemen/fine-cmdline.nvim";
       flake = false;
     };
+
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
   outputs =
-    { nixpkgs, home-manager, stylix, emacs-overlay, ... }@inputs:
+    { self, nixpkgs, home-manager, stylix, ... }@inputs:
     let
       system = "x86_64-linux";
       host = "default";
       username = "ezzobir";
-      pkgs = import nixpkgs {
-        overlays = [ emacs-overlay.overlay ];
-        inherit system;
-      };
     in
     {
       nixosConfigurations = {
