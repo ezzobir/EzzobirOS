7b683a4876996350e9a44ec01a68fc56dce3c162
installer <installer@gmail.com>
2024-09-08 23:39:55 +0100

    make emacs configs independent

--- config/emacs.nix
+++ config/emacs.nix
@@ -6,22 +6,5 @@
   programs.emacs = {
     enable = true;
     package = pkgs.emacs29-pgtk;
-    extraPackages = epkgs: with epkgs; [
-      use-package
-      vterm
-      treesit-grammars.with-all-grammars
-      evil
-      evil-collection
-      which-key
-      general
-      ivy
-      ivy-rich
-      swiper
-      counsel
-      company
-      all-the-icons
-      doom-modeline
-      nix-mode
-    ];
   };
 }
