ecf3c4c4dfef3426356327455bb855c5e0d3c35d
installer <installer@gmail.com>
2024-09-08 19:16:21 +0100

    emacs-overlay

--- config/emacs.nix
+++ config/emacs.nix
@@ -6,24 +6,29 @@
 
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs29-pgtk;
-    extraPackages = epkgs: with epkgs; [
-      vterm
-      treesit-grammars.with-all-grammars
-      evil
-      evil-collection
-      which-key
-      general
-      ivy
-      ivy-rich
-      swiper
-      counsel
-      company
-      all-the-icons
-      doom-modeline
-      nix-mode
-    ];
-    config = ./emacs/emacs.el;
+    package =
+      (pkgs.emacsWithPackagesFromUsePackage {
+        config = ../config/emacs/emacs.el;
+        defaultInitFile = true;
+        package = pkgs.emacs29-pgtk;
+        extraPackages = epkgs: with epkgs; [
+          vterm
+          treesit-grammars.with-all-grammars
+          evil
+          evil-collection
+          which-key
+          general
+          ivy
+          ivy-rich
+          swiper
+          counsel
+          company
+          all-the-icons
+          doom-modeline
+          nix-mode
+        ];
+        config = ./emacs/emacs.el;
+      });
     # extraConfig = ''
     # '';
   };
