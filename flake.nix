e1a30209f6d5be6efff3c554badbe86ca8681603
installer <installer@gmail.com>
2024-09-08 20:24:57 +0100

    delete emacs-overlay

--- flake.nix
+++ flake.nix
@@ -4,24 +4,19 @@
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     # home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     # stylix.url = "github:danth/stylix/release-24.05";
     stylix.url = "github:danth/stylix";
-
     fine-cmdline = {
       url = "github:VonHeikemen/fine-cmdline.nvim";
       flake = false;
     };
-
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
   outputs =
-    { self, nixpkgs, home-manager, stylix, ... }@inputs:
+    { nixpkgs, home-manager, stylix, ... }@inputs:
     let
       system = "x86_64-linux";
       host = "default";
@@ -31,7 +26,7 @@
       nixosConfigurations = {
         "${host}" = nixpkgs.lib.nixosSystem {
           specialArgs = {
-	          inherit system;
+	    inherit system;
             inherit inputs;
             inherit username;
             inherit host;
