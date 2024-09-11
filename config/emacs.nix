e9c7dbb10abd0f63372b6a88a5d3894eed2afd1f
installer <installer@gmail.com>
2024-09-11 20:01:19 +0100

    add emacs configs and packages

--- config/emacs.nix
+++ config/emacs.nix
@@ -258,12 +258,13 @@
 
     (use-package ivy-rich
       :after ivy
-      :init
+      :config
       (ivy-rich-mode 1))
 
     (use-package all-the-icons-ivy-rich
       :after ivy-rich
-      :init (all-the-icons-ivy-rich-mode 1))
+      :config
+      (all-the-icons-ivy-rich-mode 1))
 
     (use-package counsel
       :custom
@@ -342,21 +343,20 @@
 
     ;; Which key
     (use-package which-key
-    :init
-      (which-key-mode 1)
     :config
-    (setq which-key-side-window-location 'bottom
-      which-key-sort-order #'which-key-key-order-alpha
-      which-key-sort-uppercase-first nil
-      which-key-add-column-padding 1
-      which-key-max-display-columns nil
-      which-key-min-display-lines 6
-      which-key-side-window-slot -10
-      which-key-side-window-max-height 0.25
-      which-key-idle-delay 0.8
-      which-key-max-description-length 25
-      which-key-allow-imprecise-window-fit t
-      which-key-separator " → " ))
+      (which-key-mode 1)
+      (setq which-key-side-window-location 'bottom
+        which-key-sort-order #'which-key-key-order-alpha
+        which-key-sort-uppercase-first nil
+        which-key-add-column-padding 1
+        which-key-max-display-columns nil
+        which-key-min-display-lines 6
+        which-key-side-window-slot -10
+        which-key-side-window-max-height 0.25
+        which-key-idle-delay 0.8
+        which-key-max-description-length 25
+        which-key-allow-imprecise-window-fit t
+        which-key-separator " → " ))
     '';
   };
 }
