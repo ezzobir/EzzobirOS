bfc338484072cb8ffeba34cb48d65d627b107553
installer <installer@gmail.com>
2024-09-12 15:18:06 +0100

    add dired configuration

--- config/emacs.nix
+++ config/emacs.nix
@@ -37,6 +37,7 @@
       company-box
       dired-open
       peep-dired
+      neotree
       doom-modeline
       nix-mode
     ];
@@ -239,6 +240,7 @@
         (setq dired-open-extensions '(("gif" . "nsxiv")
                                       ("jpg" . "imv")
                                       ("png" . "imv")
+                                      ("pdf" . "zathura")
                                       ("mkv" . "mpv")
                                       ("mp4" . "mpv"))))
 
@@ -367,6 +369,24 @@
       ;; Language support
       (use-package python-mode)
       (use-package lua-mode)
+
+      ;; neotree
+      (use-package neotree
+        :config
+        (setq neo-smart-open t
+              neo-show-hidden-files t
+              neo-window-width 55
+              neo-window-fixed-size nil
+              inhibit-compacting-font-caches t
+              projectile-switch-project-action 'neotree-projectile-action) 
+              ;; truncate long file names in neotree
+              (add-hook 'neo-after-create-hook
+                 #'(lambda (_)
+                     (with-current-buffer (get-buffer neo-buffer-name)
+                       (setq truncate-lines t)
+                       (setq word-wrap nil)
+                       (make-local-variable 'auto-hscroll-mode)
+                       (setq auto-hscroll-mode nil)))))
 
       ;; org mode
       (use-package org
