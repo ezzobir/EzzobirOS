a4d672254e87ad1a800c6301a41d8b715ba49aa8
installer <installer@gmail.com>
2024-09-12 21:58:04 +0100

    disable counsel app launcher

--- config/emacs.nix
+++ config/emacs.nix
@@ -172,24 +172,24 @@
       (my/leader-keys
         "SPC" '(evil-switch-to-windows-last-buffer :which-key "last buffer"))
 
-      ;; emacs-counsel-launcher
-      (defun emacs-counsel-launcher ()
-        "Create and select a frame called emacs-counsel-launcher which consists only of a minibuffer and has specific dimensions. Runs counsel-linux-app on that frame, which is an emacs command that prompts you to select an app and open it in a dmenu like behaviour. Delete the frame after that command has exited"
-        (interactive)
-        (with-selected-frame 
-          (make-frame '((name . "emacs-run-launcher")
-                        (minibuffer . only)
-                        (fullscreen . 0) ; no fullscreen
-                        (undecorated . t) ; remove title bar
-                        ;;(auto-raise . t) ; focus on this frame
-                        ;;(tool-bar-lines . 0)
-                        ;;(menu-bar-lines . 0)
-                        (internal-border-width . 10)
-                        (width . 80)
-                        (height . 11)))
-                        (unwind-protect
-                          (counsel-linux-app)
-                          (delete-frame))))
+      ;; ;; emacs-counsel-launcher
+      ;; (defun emacs-counsel-launcher ()
+      ;;   "Create and select a frame called emacs-counsel-launcher which consists only of a minibuffer and has specific dimensions. Runs counsel-linux-app on that frame, which is an emacs command that prompts you to select an app and open it in a dmenu like behaviour. Delete the frame after that command has exited"
+      ;;   (interactive)
+      ;;   (with-selected-frame 
+      ;;     (make-frame '((name . "emacs-run-launcher")
+      ;;                   (minibuffer . only)
+      ;;                   (fullscreen . 0) ; no fullscreen
+      ;;                   (undecorated . t) ; remove title bar
+      ;;                   ;;(auto-raise . t) ; focus on this frame
+      ;;                   ;;(tool-bar-lines . 0)
+      ;;                   ;;(menu-bar-lines . 0)
+      ;;                   (internal-border-width . 10)
+      ;;                   (width . 80)
+      ;;                   (height . 11)))
+      ;;                   (unwind-protect
+      ;;                     (counsel-linux-app)
+      ;;                     (delete-frame))))
 
       ;; all-the-icons
       (use-package all-the-icons
